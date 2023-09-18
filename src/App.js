3a65760ece429072c894fab8d9de46f37e9c7f71
annk1995 <kabuikariuki052@gmail.com>
2023-09-18 18:37:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const images = [
+  'image1.jpg',
+  'image2.jpg',
+  'image3.jpg',
+  'image4.jpg',
+  // Add more image filenames here
+];
+
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Image Carousel</h1>
+      <div className="carousel">
+        <button onClick={prevImage}>Previous</button>
+        <img src={images[currentIndex]} alt={`Image ${currentIndex + 1}`} />
+        <button onClick={nextImage}>Next</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
